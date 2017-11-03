55500349626f770c8fdc92b0d5277c017badd4a5
jdr023 <jdr000@live.com>
2017-11-02 18:41:16 -0700

    Add mine route

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-from flask import Flask
 from core import Blockchain
+from flask import Flask, jsonify
+
+
+instance = Flask(__name__)
+blockchain = Blockchain()
+
+
+@instance.route('/mine', methods=['GET'])
+def mine():
+    pass
 
 
 if __name__ == '__main__':
-    blockchain = Blockchain()
-
-    # Start local server
-    instance = Flask(__name__)
     instance.run()
